5eb6e0a68af2a2eb91758c5e5fbbc335dcec761e
Jihoo Park <park0588@algonquinlive.com>
2023-06-22 19:20:41 -0400

    Week6 part 1

--- app/src/main/java/algonquin/cst2335/park0588/ChatRoom.java
+++ app/src/main/java/algonquin/cst2335/park0588/ChatRoom.java
@@ -1,8 +1,12 @@
 package algonquin.cst2335.park0588;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 
 import algonquin.cst2335.park0588.databinding.ActivityChatRoomBinding;
 
@@ -13,5 +17,28 @@ ActivityChatRoomBinding binding;
         super.onCreate(savedInstanceState);
         binding = ActivityChatRoomBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        binding.rv.setAdapter(new RecyclerView.Adapter<MyRowHolder>() {
+            @NonNull
+            @Override
+            public MyRowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return null;
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull MyRowHolder holder, int position) {
+
+            }
+
+            @Override
+            public int getItemCount() {
+                return 0;
+            }
+        });
+    }
+
+    class MyRowHolder extends RecyclerView.ViewHolder{
+        public MyRowHolder(@NonNull View itemView){
+            super(itemView);
+        }
     }
 }
